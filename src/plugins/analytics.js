17a2acc9828b8d1fb9a783bf4ed224f92f664291
arklow.sei <raiceadship@gmail.com>
2024-11-06 16:41:42 +0000

    Update analytics.js

--- src/plugins/analytics.js
+++ src/plugins/analytics.js
@@ -1,7 +1,11 @@
-import { createGtag } from 'vue-gtag-next'
+import VueGtag from 'vue-gtag-next'
 
-export const analytics = createGtag({
-  property: {
-    id: 'G-NQLTJY5B7B' // Replace with your GA4 Measurement ID
+export const analytics = {
+  install(app) {
+    app.use(VueGtag, {
+      property: {
+        id: 'G-NQLTJY5B7B' // Replace with your GA4 Measurement ID
+      }
+    })
   }
-}) 
+} 
